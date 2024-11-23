e50f744479a05f26b71a1d81f4c94542aa1d1b3e
Emir <emirawp@hotmail.com>
2024-11-23 07:37:07 +0300

    Convert ES Module

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,15 @@
-module.exports = {
-  apps: [
-    {
-      name: "squirrelquest",
-      script: "bun",
-      args: "start",
-      env: {
-        NODE_ENV: "production",
-        PORT: "3000",
-      },
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "1G",
-      exp_backoff_restart_delay: 100,
+export const apps = [
+  {
+    name: "squirrelquest",
+    script: "bun",
+    args: "start",
+    env: {
+      NODE_ENV: "production",
+      PORT: "3000",
     },
-  ],
-};
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    exp_backoff_restart_delay: 100,
+  },
+];
