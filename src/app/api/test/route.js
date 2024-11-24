8c1150e33f934cc6031863f4288c6d5a6963aedc
Emir <emirawp@hotmail.com>
2024-11-24 05:20:11 +0300

    Delete Test API

--- src/app/api/test/route.js
+++
@@ -1,7 +0,0 @@
-import { createRouteHandler } from "../lib/create-route-handler";
-import handler from "../lib/express-handler";
-
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-
-export const GET = createRouteHandler(handler, "/test");
